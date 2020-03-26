01fa46300caf604b2a0e27f51aa190369e8c9cbc
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-03-27 02:12:34 +0900

    [ADD]Public,Adminコントローラー作成、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :end_users
-  devise_for :admins
+  devise_for :end_users, controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations'
+  }
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
