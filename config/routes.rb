a75c1785b2681ddde54c0912674d6efa05afdfe4
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-03-27 15:35:44 +0900

    [ADD]Devise(Public,Admin用)のView作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :end_users, controllers: {
+  devise_for :end_users,
+  path: "end_user",
+  controllers: {
     sessions: 'public/sessions',
     registrations: 'public/registrations'
   }
-  devise_for :admins, controllers: {
+  devise_for :admins,
+  path: "admin",
+  controllers: {
     sessions: 'admin/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
