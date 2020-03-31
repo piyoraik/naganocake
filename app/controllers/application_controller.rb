237106ef193f67ddaf29a897915ae1430ed8b6ee
raityupiyo <piyoraik@gmail.com>
2020-03-31 14:18:10 +0900

    [FINISH]認証機能完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,19 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource_or_scope)
-        if resource_or_scope == :admin
+        if resource_or_scope.class == Admin
             admin_path
-        elsif resource_or_scope == :end_user
-            end_user_path(current_user.id)
+        elsif resource_or_scope.class == EndUser
+            end_users_path
         end
     end
 
     def after_sign_out_path_for(resource_or_scope)
+        # binding.pry
         if resource_or_scope == :admin
             new_admin_session_path
         elsif resource_or_scope == :end_user
             new_end_user_session_path
-        else
-            root_path
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   root 'public/items#top'
 
   scope module: :public do
-    resources :end_users, only: [:show, :edit, :update]
+    resources :end_users, only: [:index, :edit, :update]
     get '/destroy_show' => 'end_uesrs#destroy_show'
   end
 
