25126fd56b6f031f8b2b4d81dd9a4bd2e72edac1
raityupiyo <piyoraik@gmail.com>
2020-03-31 16:36:43 +0900

    [UPDATE]顧客情報更新機能追加

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -10,6 +10,8 @@ class Public::EndUsersController < ApplicationController
 
     # 顧客の登録情報更新
     def update
+        current_end_user.update(end_user_params)
+        redirect_to end_users_path
     end
 
     # 顧客の退会確認画面
@@ -19,4 +21,9 @@ class Public::EndUsersController < ApplicationController
     # 顧客の退会処理
     def update_destroy
     end
+
+    private
+        def end_user_params
+            params.require(:end_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
+        end
 end
