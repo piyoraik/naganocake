d6d0abc5495a3244a94745ff097ef5eac3af1b44
raityupiyo <piyoraik@gmail.com>
2020-04-02 13:01:06 +0900

    [UPDATE]新規登録画面にブートストラップ適応、画像のプレビュー機能追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+    $('#img_prev').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#user_img").change(function(){
+        readURL(this);
+    });
+  });
