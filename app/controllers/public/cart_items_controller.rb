efd0df168b54b4a76a7b3c52a0115dd2155904cc
raityupiyo <piyoraik@gmail.com>
2020-04-04 17:29:09 +0900

    [UPDATE]cart_itemsコントローラーにアクション追加、ルート設定

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,21 @@
 class Public::CartItemsController < ApplicationController
+    # カート内商品一覧画面
+    def index
+    end
+
+    # カート内商品データ更新
+    def update
+    end
+
+    # カート内商品削除(一商品)
+    def destroy
+    end
+
+    # カート内商品削除(全部)
+    def all_destroy
+    end
+
+    # カート内商品データ追加
+    def create
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,12 @@ Rails.application.routes.draw do
   get '/items' => 'public/items#index'
   get '/items/:id' => 'public/items#show'
 
+  get '/cart_items' => 'public/cart_items#index'
+  patch '/cart_items/:id' => 'public/cart_items#update'
+  delete '/cart_items/:id' => 'public/cart_items#destroy'
+  delete '/cart_items/all' => 'public/cart_items#all_destroy'
+  post '/cart_items' => 'public/cart_items#create'
+
   scope module: :public do
     get '/end_users/edit' => 'end_users#edit'
     get '/end_users/destroy_show' => 'end_users#destroy_show'
@@ -27,8 +33,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/' => 'top#top'
-    resources :items, :except => :destroy
-    resources :end_users, :except => :destroy
+    resources :items, :except => :delete
+    resources :end_users, :except => :delete
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
