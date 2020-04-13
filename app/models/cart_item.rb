cbf51f64735aa06bc8ccb0a3634922c5fdd3827c
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-04-13 10:48:22 +0900

    [FIX]注文情報確認ページで演算が出来ない不具合を修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,7 @@ class CartItem < ApplicationRecord
     belongs_to :item
 
     def total_price(number, tax)
-        total = number * tax
+        total = number.to_i * tax.to_i
         return total
     end
 end
