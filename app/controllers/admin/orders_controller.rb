2f1d46f00bfc3c11d88f079ce5e7fc88ef80c21c
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-04-26 20:35:21 +0900

    [UPDATE]注文ステータスをセレクトボックスで変更できるように

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,4 +8,15 @@ class Admin::OrdersController < ApplicationController
         @total_tax = 0
         @order = Order.find(params[:id])
     end
+
+    def update
+        order = Order.find(params[:id])
+        order.update(order_status_params)
+        redirect_to admin_order_path(order)
+    end
+
+    private
+        def order_status_params
+            params.require(:order).permit(:status)
+        end
 end
